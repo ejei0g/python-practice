2d5fcf7cc6c8ad9d018f4e52f7b72ab015a39297
ejei0g <ejei0g@gmail.com>
2022-10-24 21:19:35 +0900

    feat: Data scrap and parsing, save

--- WebScrapper/main.py
+++ WebScrapper/main.py
@@ -1,11 +1,56 @@
-import sys
+from bs4 import BeautifulSoup
 from requests import get
 
 base_url = "https://weworkremotely.com/remote-jobs/search?term="
-job = "python"
+job = "javascript"
 
 response = get(f"{base_url}{job}")
 if response.status_code != 200:
     print("Fali requests")
 else:
-    print(response.text)
+    result = []
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all("section", class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all("li") # list like python
+        job_posts.pop()
+        for post in job_posts:
+            anchors = post.find_all('a')
+            anchor = anchors[1]
+            link = anchor['href']
+            a = anchor.find_all('span', class_="company")
+            company, kind, region = a
+            #print(company.string)
+            #print(kind.string)
+            #print(region.string)
+            title = anchor.find('span', class_="title")
+            #print(title.string)
+            #print("------")
+            # Save
+            job_data = {
+                'Company': company.string,
+                'Resion': region.string,
+                'Position': title.string
+
+            }
+            result.append(job_data)
+
+    #print(f'"{result}"')
+    for res in result:
+        print(res)
+# positional parameters
+
+#def say_hello(name, age):
+#    print(f"Hello {name} you r {age} years old")
+
+#say_hello("jyl", 11);
+
+# get "a href", company, date
+
+# object, list, length , object
+
+list_of_numbers = [1,2,3]
+first, second, third = list_of_numbers
+
+# span 에서 텍스트 추출 element.string
